dc639e1466f5defc58532e78d067b6dc0c791030
daido1976 <daido1976@gmail.com>
2022-06-11 22:22:09 +0900

    Introduce css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import classes from "./App.module.css";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1 className={classes.blue}>Hello world!</h1>
     </div>
   );
 }
