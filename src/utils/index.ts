fb2eb2d726cafc23130d0cbb3a03fcdad3c088c0
daido1976 <daido1976@gmail.com>
2022-06-25 14:58:22 +0900

    Enable to work but dirty

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,10 +9,20 @@ type PopulationForChart = {
 export const convertForChart = (
   pl: PrefecturePopulation[]
 ): PopulationForChart[] => {
-  return pl.flatMap((p) =>
+  const fm = pl.flatMap((p) =>
     p.populations.map(({ year, value }) => ({
       year,
       [p.prefCode.toString()]: value,
     }))
   );
+  return fm.reduce((acc: any, p) => {
+    const i = acc.findIndex((a: any) => a.year === p.year) as number;
+    if (i >= 0) {
+      acc.push({ ...acc[i], ...p });
+      acc.splice(i, 1);
+      return acc;
+    }
+    acc.push(p);
+    return acc;
+  }, []);
 };
