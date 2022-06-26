16b35c0ecb791e3d062bf3298eb86e1840cd4fc5
daido1976 <daido1976@gmail.com>
2022-06-26 09:28:41 +0900

    Refactor convertForChart

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,28 +2,35 @@ import { PrefecturePopulation } from "../types";
 
 type PopulationForChart = {
   year: number;
-  // TODO: Map で代替できるかもしれない
   [key: string]: number;
 };
 
-// TODO: リファクタリングする
 export const convertForChart = (
   pl: PrefecturePopulation[]
 ): PopulationForChart[] => {
+  // 一度以下のような構造に変える
+  // [
+  //   { year: 1960, key1: 5039206 },
+  //   { year: 1965, key1: 5171800 },
+  //   { year: 1970, key1: 5184287 },
+  //   { year: 1960, key2: 1426606 },
+  //   { year: 1965, key2: 1416591 },
+  //   { year: 1970, key2: 1427520 },
+  // ];
   const fm = pl.flatMap((p) =>
     p.populations.map(({ year, value }) => ({
       year,
       [p.prefCode.toString()]: value,
     }))
   );
-  return fm.reduce((acc: any, p) => {
-    const i = acc.findIndex((a: any) => a.year === p.year) as number;
+
+  return fm.reduce((acc: PopulationForChart[], p) => {
+    const i = acc.findIndex((x) => x.year === p.year);
     if (i >= 0) {
-      acc.push({ ...acc[i], ...p });
-      acc.splice(i, 1);
+      acc.splice(i, 1, { ...acc[i], ...p });
       return acc;
     }
-    acc.push(p);
-    return acc;
+
+    return [...acc, p];
   }, []);
 };
